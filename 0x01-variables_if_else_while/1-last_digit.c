26ff18528e0f30166a3651dddb3c581069409bf4
dii-glori <chidinmaamanze20@gmail.com>
2022-07-01 16:14:05 -0700

    Positive anything is better than negative nothing

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int c;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+
