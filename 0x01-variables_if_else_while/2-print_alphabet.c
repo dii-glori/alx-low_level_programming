f67ae9dab7a10392678a072bdb54088b1cb079b5
dii-glori <chidinmaamanze20@gmail.com>
2022-07-01 18:37:50 -0700

    message

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	/* putchar - write c to stout */
+
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+
+	putchar(c);
+	putchar('\n');
+
+	return (0);
+}
