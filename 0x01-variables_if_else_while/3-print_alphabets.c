098bcdc4374083073d3dc1a2c5dd7b99c1d5bf71
dii-glori <chidinmaamanze20@gmail.com>
2022-07-01 18:57:16 -0700

    commit again

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * 
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+
+{
+	/* putchar - write c to stout */
+
+	int c;
+	int a;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	for (a = 'A'; a <= 'Z'; a++)
+		putchar(a);
+
+	putchar('\n');
+	return (0);
+}
