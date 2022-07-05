66e7571e75b164ffdf829e98a056dc551e6a3827
dii-glori <chidinmaamanze20@gmail.com>
2022-07-05 05:27:26 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _putchar - writes the character to stdout
+ * @c: character to be used by _putchar
+ *
+ * Return: On success 0
+ */
+int _putchar(char c)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
