384add20d11a0f01c96081282e13f3c187d40aef
dii-glori <chidinmaamanze20@gmail.com>
2022-07-07 14:38:54 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,13 +7,21 @@
 
 void print_number(int n)
 {
+	unsigned int n1;
+
 	if (n < 0)
 	{
+		n1 = -n;
 		_putchar('-');
-		n = -n;
+	}
+	else
+	{
+		n1 = n;
 	}
 
-	if (n / 10 != 0)
+	if (n / 10)
+	{
 		print_number(n1 / 10);
+	}
 	_putchar(n % 10 + '0');
 }
