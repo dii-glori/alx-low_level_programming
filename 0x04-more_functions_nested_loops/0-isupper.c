0083c162a779df641fffcef3d7a9981ed2714246
dii-glori <chidinmaamanze20@gmail.com>
2022-07-07 03:25:51 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * checks for uppercase character
+ *
+ * Return: 1 or 0.
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
