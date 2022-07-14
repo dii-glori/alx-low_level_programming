8f2c9ab3a64e72dce551d6d798bcb3fd972a0e39
dii-glori <chidinmaamanze20@gmail.com>
2022-07-14 14:11:12 -0700

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: a number
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	print_helper((unsigned int)n);
+}
+
+/**
+ * print_helper - prints an unsigned integer
+ * @n: an unsigned integer to print
+ */
+
+void print_helper(unsigned int n)
+{
+	if (n < 10)
+	{
+		_putchar(n + '0');
+	}
+	else
+	{
+		print_helper(n / 10);
+		_putchar((n % 10) + '0');
+	}
+}
