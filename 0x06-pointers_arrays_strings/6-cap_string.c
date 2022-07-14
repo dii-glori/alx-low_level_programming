538d79bc7aef8a098d7daa05beead2a1330c3c21
dii-glori <chidinmaamanze20@gmail.com>
2022-07-14 10:52:28 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -23,9 +23,8 @@ char *cap_string(char *s)
 		{
 			if (s[i] == sep[j])
 			{
-				++i;
-				if (s[i] >= 'a' && s[i] <= 'z')
-					s[i] -= 32;
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+					s[i + 1] -= 32;
 				continue;
 			}
 		}
