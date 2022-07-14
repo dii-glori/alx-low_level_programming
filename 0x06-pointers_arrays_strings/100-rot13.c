2de870e3873e870c2e4791014dd98cfc3127c291
dii-glori <chidinmaamanze20@gmail.com>
2022-07-14 12:45:59 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13.
+ * @s: the sring
+ * Return: character
+ */
+char *rot13(char *s)
+{
+	int i;
+	int j;
+	char alpha[] = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+	int index = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; alpha[j] != '\0'; j++)
+		{
+			if (s[i] == alpha[j])
+			{
+				index = (j + 26) % 52;
+				s[i] = alpha[index];
+			}
+		}
+	}
+	return (s);
+}
