40f35cb61482b88b21b0ee4a64de4db67d554dd1
dii-glori <chidinmaamanze20@gmail.com>
2022-07-14 09:33:40 -0700

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @s: a string
+ * Return: a char
+ */
+char *cap_string(char *s)
+{
+	int i;
+	char sep[] = ",;.!?(){}\n\t\" ";
+	int j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (i == 0)
+		{
+			if (s[i] >= 'a' && s[i] <= 'z')
+				s[i] -= 32;
+			continue;
+		}
+		for (j = 0; sep[j] != '\0'; j++)
+		{
+			if (s[i] == sep[j])
+			{
+				if (s[i] >= 'a' && s[i] <= 'z')
+					s[i] -= 32;
+				continue;
+			}
+		}
+		if (s[i] >= 'A' && s[i] <= 'Z')
+				s[i] += 32;
+		else
+			continue;
+	}
+	return (s);
+}
