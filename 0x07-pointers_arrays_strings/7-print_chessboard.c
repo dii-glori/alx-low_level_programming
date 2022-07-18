ed2dbd5639123dc958fc88be4639a4b5acfb3fa9
dii-glori <chidinmaamanze20@gmail.com>
2022-07-18 11:47:36 -0700

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,16 +8,12 @@ void print_chessboard(char (*a)[8])
 {
 	int i, j;
 
-	i = 0;
-	while (i < 8)
+	for (i = 0; i < 8; i++)
 	{
-		j = 0;
-		while (j < 8)
+		for (j = 0; j < 8; j++)
 		{
 			_putchar(a[i][j]);
-			j++;
 		}
 		_putchar('\0');
-		i++;
 	}
 }
