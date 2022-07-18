ad4159495286783a0c34d74fb61655c0474da7a0
dii-glori <chidinmaamanze20@gmail.com>
2022-07-18 05:49:04 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _strchr - locates a character in a string.
@@ -16,5 +17,5 @@ char *_strchr(char *s, char c)
 			return (&s[i]);
 	}
 
-	return (0);
+	return (NULL);
 }
