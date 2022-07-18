c563ff9992f17399222deb875d55c96f8ab85fa8
dii-glori <chidinmaamanze20@gmail.com>
2022-07-18 06:17:01 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,5 +8,24 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
+	int i, j;
+	unsigned int count = 0;
 
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+				count++;
+			j++;
+		}
+		i++;
+
+		if (accept[j] == '\0')
+			break;
+	}
+
+	return (count);
 }
