d854bf9751429ee41735e898a75554f97b4b6858
dii-glori <chidinmaamanze20@gmail.com>
2022-07-18 05:44:50 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,16 +10,11 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	i = 0;
-	while (s[i] != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] != c)
-			i++;
-		else
+		if (s[i] == c)
 			return (s);
 	}
-	if (c == '\0')
-		return (s);
 
 	return ('\0');
 }
