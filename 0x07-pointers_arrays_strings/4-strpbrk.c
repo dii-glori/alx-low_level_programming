decf058eafbeafd337004fd5e1fab1c24f12f0b9
dii-glori <chidinmaamanze20@gmail.com>
2022-07-18 06:45:09 -0700

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -15,12 +15,9 @@ char *_strpbrk(char *s, char *accept)
 	i = 0;
 	while (s[i] != '\0')
 	{
-		j = 0;
-		while (accept[j] != '\0')
+		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if (s[i] != accept[j])
-				j++;
-			else
+			if (s[i] == accept[j])
 				return (&s[i]);
 		}
 		i++;
