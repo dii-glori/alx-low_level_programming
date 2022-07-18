b4d9fe29a07b9ef46010cacd44930eead9e27113
dii-glori <chidinmaamanze20@gmail.com>
2022-07-18 06:40:51 -0700

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,12 +1,31 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _memset - fills memory with a constant byte.
- * @s: memory space pointer
- * @b: constant byte
- * @n: number of bytes
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: a string
+ * @accept: set of bytes
+ * Return: a pointer to the byte in s that matches one of the bytes in accept,
+ * or NULL if no such byte is found
  */
-char *_memset(char *s, char b, unsigned int n)
+char *_strpbrk(char *s, char *accept)
 {
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (s[i] != accept[j])
+				j++;
+			else
+				return (&s[i]);
+		}
+		i++;
+	}
+
+	return (NULL);
 
 }
