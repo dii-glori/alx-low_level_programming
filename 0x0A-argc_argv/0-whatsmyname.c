b18706b481290b9f56b7f60ee89f19cad8a6872d
dii-glori <chidinmaamanze20@gmail.com>
2022-07-22 08:49:50 -0700

    print the programme name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: number of command line arguments
+ * @argv: array containing the program command line argument
+ *
+ * Return: always 0 success
+ */
+int main(int argc char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
