a9e7dca7e72278f2324ac4316a933a2b09ca2705
dii-glori <chidinmaamanze20@gmail.com>
2022-07-22 09:47:29 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - entry point
@@ -14,7 +15,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-	printf("%d", argv[1] * argv[2]);
+	printf("%d", atoi(argv[1]) * atoi(argv[2]));
 
 	return (0);
 }
