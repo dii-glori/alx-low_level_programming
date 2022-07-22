8b795a05a0200d6d476bd6ea25784f2877de25ff
dii-glori <chidinmaamanze20@gmail.com>
2022-07-22 09:40:40 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: argument array
+ *
+ * Return: always 0 success
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d", argv[1] * argv[2]);
+
+	return (0);
+}
