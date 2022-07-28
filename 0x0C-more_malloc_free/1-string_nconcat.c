372ac2d87de8cf10ea7af4c3b75e3aaec02fddc1
dii-glori <chidinmaamanze20@gmail.com>
2022-07-28 05:19:48 -0700

    i cant anymore

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,21 +1,19 @@
 #include "main.h"
-#include <stdlib.h>
-#include <string.h>
+#include<stdlib.h>
+#include<stddef.h>
+#include<string.h>
 
 /**
- * string_nconcat - a function that concatenates two strings.
- * @s1: first string
- * @s2: second string
- * @n: size in bytes
- *
- * Return: a pointer
+ * string_nconcat - form a new string from two strings
+ * @s1: pointer to first string
+ * @s2: pointer to second string
+ * @n:  number of bytes to copy from s2
+ * Return: pointer to new or NULL
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *ar;
-	unsigned int n1;
-	unsigned int n2;
-	unsigned int i;
+	unsigned int n1, n2, i = 0;
+	char *arr;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -24,21 +22,20 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	n1 = strlen(s1);
 	n2 = strlen(s2);
 	n2 = (n < n2) ? n : n2;
-
-	ar = malloc(n1 + n2 + 1);
-	if (ar == NULL)
+	arr = malloc((n1 + n2 + 1) * sizeof(char));
+	if (!arr)
 		return (NULL);
 	while (i < n1)
 	{
-		ar[i] = s1[i];
+		arr[i] = s1[i];
 		i++;
 	}
 	i = 0;
 	while (i < n2)
 	{
-		ar[n1 + i] = s2[i];
+		arr[n1 + i] = s2[i];
 		i++;
 	}
-	ar[n1 + i] = '\0';
-	return (ar);
+	arr[n1 + i] = '\0';
+	return (arr);
 }
