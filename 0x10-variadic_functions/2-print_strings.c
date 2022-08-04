924d3e2ad22c9ea5885397d9fb27d5e32a7441fc
dii-glori <chidinmaamanze20@gmail.com>
2022-08-04 08:37:46 -0700

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,18 +1,31 @@
 #include "variadic_functions.h"
 
-void is_digit(int 
 /**
  * print_strings - prints strings, followed by a new line.
- * @separator: string to be printed between the strings
- * @n: number of strings passed to the function
+ * @separator: string to be printed between numbers
+ * @n: number of integers passed to the function
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list ap;
 
-	unsigned int i, j;
+	unsigned int i;
+	unsigned int a = n - 1;
+	char *str;
 
-	if (separator == NULL)
-		return;
+	va_start(ap, n);
 
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(ap, char*);
+		if (str == NULL)
+			printf("(nil)");
+		else
+			printf("%s", str);
+
+		if (separator != NULL && i < a)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(ap);
 }
