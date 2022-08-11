5dabee2ea1712cd47b0325205cde1bb8bedc5203
dii-glori <chidinmaamanze20@gmail.com>
2022-08-11 11:22:11 -0700

    101-hello_holberton.nasm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,10 +1,10 @@
 global    main
-          extern    puts
+          extern    printf
 
           section   .text
-main:                                       ; This is called by the C library startup code
-          mov       rdi, message            ; First integer (or pointer) argument in rdi
-          call      puts                    ; puts(message)
-          ret                               ; Return from main back into C library wrapper
+main:
+          mov       rdi, message
+          call      printf
+          ret
 message:
-          db        "Hello, Holberton", 0        ; Note strings must be terminated with 0 in C
+          db        "Hello, Holberton", 0
