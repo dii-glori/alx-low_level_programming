a9fb84fe254b6e336cb2c28b93ca1cebcd50c1a0
dii-glori <chidinmaamanze20@gmail.com>
2022-08-16 09:34:23 -0700

    fixes

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -35,5 +35,7 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
+int is_visited(const listint_t *, const listint_t **, int);
+listint_t *find_listint_loop(listint_t *head);
 
 #endif /* LISTS_H */
